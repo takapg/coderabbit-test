81cf8998bf2953109dceba56d28a075268e01e6e
takapg <75102730+takapg@users.noreply.github.com>
2026-05-10 16:38:41 +0900

    Refactor greet method for string interpolation

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,11 +1,13 @@
-def greet(name)
-  msg = "Hello, " + name
+# frozen_string_literal: true
 
-  if name == "admin"
-    puts msg.upcase()
+def greet(name)
+  msg = "Hello, #{name}"
+
+  if name == 'admin'
+    puts msg.upcase
   else
     puts msg
   end
 
-  return msg
+  msg
 end
