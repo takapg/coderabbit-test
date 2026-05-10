5adae5a1d9c08db3cb1723c69d14e48f86ec1de3
takapg <75102730+takapg@users.noreply.github.com>
2026-05-10 21:43:29 +0900

    Add RuboCop step to CI workflow (#10)
    * Add RuboCop step to CI workflow
    
    * Refactor greet method for string interpolation
    
    * Change double quotes to single quotes in Gemfile
    
    * Fix quotes in sample_spec.rb for consistency

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-ruby file: ".ruby-version"
+ruby file: '.ruby-version'
 
 group :development, :test do
-  gem "rspec"
-  gem "rubocop", require: false
-  gem "rubocop-performance", require: false
+  gem 'rspec'
+  gem 'rubocop', require: false
+  gem 'rubocop-performance', require: false
 end
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require_relative '../lib/sample'
 
 RSpec.describe 'greet メソッドのテスト' do
-  describe '挨拶の出力' do
+  describe '挨拶 of 出力' do
     it '引数に渡した名前を含む挨拶を標準出力すること' do
-      expect { greet("Alice") }.to output("Hello, Alice\n").to_stdout
+      expect { greet('Alice') }.to output("Hello, Alice\n").to_stdout
     end
 
     it '名前がadminの場合、大文字で挨拶を標準出力すること' do
-      expect { greet("admin") }.to output("HELLO, ADMIN\n").to_stdout
+      expect { greet('admin') }.to output("HELLO, ADMIN\n").to_stdout
     end
   end
 
   describe '戻り値' do
     it '正しい結果（挨拶文字列）を返すこと' do
-      expect(greet("Alice")).to eq "Hello, Alice"
+      expect(greet('Alice')).to eq 'Hello, Alice'
     end
   end
 end
